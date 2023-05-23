fbe1365c8a4a62ab6d0f738867d3cbd6a2c520b8
Luan Vuong <luanvuong@orgos.net>
2023-05-23 14:20:02 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import styled from 'styled-components';
+// import { useState } from 'react';
+// import { Counter2 } from 'components/Counter';
+import { Button } from 'components/Button';
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Button percent={10} onClick={()=>console.log("Clickkkk")}><span>Click</span>Me</Button>
+      <Button boderColor="red" percent={-109}>109%</Button>
+      <Button textColor="white" bgColor="transparent" boderColor="white">0%</Button>
+      <Button width={77} height={32} textColor="white" bgColor="#5429FF">Following</Button>
+      <Button width={77} height={32} textColor="#5429FF" bgColor="rgba(84, 41, 255, 0.1)">Follow</Button>
+      
       </header>
     </div>
   );
