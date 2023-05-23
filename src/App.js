8613990ef66557bf5d8b71578d04bf89989ed32d
Luan Vuong <luanvuong@orgos.net>
2023-05-23 15:31:57 +0700

    card component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import './App.css';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 // import { useState } from 'react';
 // import { Counter2 } from 'components/Counter';
+
 import { Button } from 'components/Button';
+import Card from 'components/Card';
+import plusIcon from 'assets/plus.svg'
+import rightArrow from 'assets/right-arrow.svg'
+const AppWrapper = styled.div`
+  text-align: center;
+  .button-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 14px 16px;
+    font-weight: 700;
+    font-size: 13px;
+    color: #27262E;
+  }
+`;
 function App() {
 
   return (
-    <div className="App">
+    <AppWrapper>
       <header className="App-header">
-      <Button percent={10} onClick={()=>console.log("Clickkkk")}><span>Click</span>Me</Button>
-      <Button boderColor="red" percent={-109}>109%</Button>
-      <Button textColor="white" bgColor="transparent" boderColor="white">0%</Button>
-      <Button width={77} height={32} textColor="white" bgColor="#5429FF">Following</Button>
-      <Button width={77} height={32} textColor="#5429FF" bgColor="rgba(84, 41, 255, 0.1)">Follow</Button>
-      
+      <Card title={"Revenue"} amount="5.00" percent={12.3}></Card>
+      {/* <Button 
+        width={196}
+        height={49}
+        textColor="black"
+        borderRadius="12px"
+      >
+        <div className="button-content">
+          <img src={plusIcon} alt="plus-icon"></img>
+          Top Up Balance
+          <img src={rightArrow} alt="arrow-icon"></img>
+        </div>
+        </Button> */}
       </header>
-    </div>
+    </AppWrapper>
   );
 }
 
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,7 +6,7 @@ const StyledButton = styled.button`
   width: ${props => `${props.width}px`};
   height: ${props => `${props.height}px`};
   border: ${props => props.boderColor ? `1px solid ${props.boderColor}` : "none"};
-  border-radius: 40px;
+  border-radius: ${props => props.borderRadius};
   background: ${props => props.bgColor};
   font-weight: 500;
   line-height: 21px;
@@ -16,7 +16,7 @@ const StyledButton = styled.button`
     margin-right: 8px;
   }
 `;
-export const Button = ({ width, height, textColor, bgColor, boderColor, percent,fontSize, children, ...rest }) => {
+export const Button = ({ width, height, textColor, bgColor, boderColor, percent, fontSize, borderRadius, children, ...rest }) => {
   return <StyledButton 
     percent={percent}
     width={width}
@@ -25,6 +25,7 @@ export const Button = ({ width, height, textColor, bgColor, boderColor, percent,
     bgColor={bgColor}
     boderColor={boderColor}
     fontSize={fontSize}
+    borderRadius={borderRadius}
     {...rest}
     >
       { percent && <img src={ percent< 0 ? redArrow : greenArrow} alt="arr"></img> }
@@ -36,5 +37,6 @@ export const Button = ({ width, height, textColor, bgColor, boderColor, percent,
   textColor: '#5429FF',
   width: 128,
   height: 46,
+  borderRadius: '40px',
   fontSize: 16,
 };
